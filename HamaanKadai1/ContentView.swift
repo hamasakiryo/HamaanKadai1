d3669ab876a33bef678aa2e934ce71af157dea85
hamasakiryo <hamaan.0927@icloud.com>
2024-01-22 16:34:26 +0900

    first commit

--- HamaanKadai1/ContentView.swift
+++ HamaanKadai1/ContentView.swift
@@ -5,19 +5,61 @@
 //  Created by 浜崎良 on 2024/01/22.
 //
 
+
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var num1 = ""
+    @State private var num2 = ""
+    @State private var num3 = ""
+    @State private var num4 = ""
+    @State private var num5 = ""
+    @State private var labelText = "Label"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("", text: $num1)
+                .textFieldModifier()
+            TextField("", text: $num2)
+                .textFieldModifier()
+            TextField("", text: $num3)
+                .textFieldModifier()
+            TextField("", text: $num4)
+                .textFieldModifier()
+            TextField("", text: $num5)
+                .textFieldModifier()
+
+            Button("Button") {
+                let nums = [num1, num2, num3 ,num4, num5]
+                
+                let total = nums.reduce(0) { result, num in
+                    result + (Int(num) ?? 0)
+                }
+                labelText = String(total)
+            }
+            .padding()
+            Text(labelText)
         }
-        .padding()
     }
 }
+
+struct TextfieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .textFieldStyle(.roundedBorder)
+            .frame(width: 100)
+            .keyboardType(.numberPad)
+    }
+}
+
+extension View {
+    func textFieldModifier() -> some View {
+        modifier(TextfieldModifier())
+    }
+}
+//正直、'body()'や'textFieldModifier()'メソッドの内部的な理解はできていない。
+//'ViewModifier'は同じモディファイアが重複するとコードが冗長になるため、それを解消するために使うといった表面的な理解しかできていない。
 
 #Preview {
     ContentView()
